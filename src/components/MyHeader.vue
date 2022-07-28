b5f39034d9fa12633be5b4ad5cc8ebfb7ab8720d
ManuelTano <manueltano98@gmail.com>
2022-07-28 18:14:37 +0200

    created components / sass / bootstrap / axios

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyHeader',
+        props: {
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+</style> 
